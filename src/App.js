5c040cb381368eaaa76afc7ddc50bed2fb93579d
alm <alm.dev2@gmail.com>
2020-05-26 19:53:09 +0200

    commit menu client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'antd/dist/antd.css';
+import { Button, Dropdown, Typography, Menu, Space } from 'antd';
+import { PlusOutlined, DownOutlined } from '@ant-design/icons'
+
+import './App.css'
+
+const { Title, Text } = Typography;
+
+const menu = (
+  <Menu>
+    <Menu.Item key="0">
+      Pause
+    </Menu.Item>
+    <Menu.Item key="1">
+      Resume
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="3">
+      Delete
+    </Menu.Item>
+  </Menu>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '10px 250px' }}>
+      <div className="row-container">
+        <h3 style={{ marginRight: "auto" }}>Torrent Feed</h3>
+        <Space>
+          <Dropdown overlay={menu} trigger={['click']}>
+            <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+              Actions <DownOutlined />
+            </a>
+          </Dropdown>
+          <Button>
+            <PlusOutlined /> TORRENT
+        </Button>
+        </Space>
+      </div>
     </div>
   );
 }
